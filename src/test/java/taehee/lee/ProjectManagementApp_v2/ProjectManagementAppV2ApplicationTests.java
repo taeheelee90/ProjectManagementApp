2855c9661f59f12d679a4744d9b30d3a45a66bb6
taeheelee90 <taehee.lee90@gmail.com>
2020-12-08 17:01:15 +0200

    Preparing V2

---
+++ src/test/java/taehee/lee/ProjectManagementApp_v2/ProjectManagementAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package taehee.lee.ProjectManagementApp_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
